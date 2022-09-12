5252587065b973129239902a0482ea1775f680ff
Gragg, Gabrielle <111663396+ggragg00@users.noreply.github.com>
2022-09-11 21:50:53 -0400

    Create examples_01-10.js

---
+++ Scripts/examples_01-10.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var welcome = "Welcome to YellowLoss LLC!";
+var welcome2 = "We are a wellness the specializes in making products for your health with your health in mind!"
+
+alert(welcome);
+alert(welcome2);
